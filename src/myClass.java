f083d56bc9affd165127a0b39946ad09c58c57f4
seoyoungkim <seoyoungkim@192.168.0.137>
2017-04-10 11:54:47 +0900

    d

--- src/myClass.java
+++ src/myClass.java
@@ -1,12 +1,16 @@
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class myClass extends JPanel {
+	private static final long serialVersionUID = 1L;
+	public static Dimension size;
 	
 	public myClass() {
 		super();
@@ -14,7 +18,6 @@ public class myClass extends JPanel {
 		//Color myColor = new Color(255 ,255, 255, 100);
 		setBackground(Color.BLACK);
 	}
-	
 	@Override
 	public void paintComponent (Graphics g) {
 		super.paintComponent(g);
@@ -24,23 +27,41 @@ public class myClass extends JPanel {
 				BasicStroke.JOIN_ROUND));
 		g.setColor(Color.WHITE);
 		
-		g2.drawLine(0, 0, 1000, 1000);
-		g2.drawLine(1000, 0, 0, 1000);
+		//Rectangle2D r = new Rectangle2D.Double(100, 200, 100, 100);
+		//fill. screen with rectangles
+		int boxesWidth = 25;
+		int boxesHeight = 13;
 		
-		for(int x = 0; x < 2880; x+=25) {	
-		g2.drawLine(x, 0, 1680, 1800);
+		for(int x = 0; x <boxesWidth; x++) {
+			for(int y = 0; y < boxesHeight; y++) {
+				
+				int v = (int)((double)255 / (double)(boxesWidth + boxesHeight) * (x + y));
+						
+				Color c = new Color(0, v, 225, v); //RGBA - Red Green Blue Alpha, 0~255
+				g2.setColor(c);
+				
+				//instantiate our rectangle
+				Rectangle2D r = new Rectangle2D.Double(
+						x * size.getWidth()  / boxesWidth,
+						y * size.getHeight() / boxesHeight,
+						size.getWidth()  / boxesWidth,
+						size.getHeight() / boxesHeight
+						);
+				//Draw our rectangle
+				g2.fill(r);
+				
+			}
+		}
 	}
 		
-	}
-
 	public void setupDisplay() {
 		JFrame application = new JFrame();
 		application.add(this);
 		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		application.setSize(2880, 1800);
 		application.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		application.setUndecorated(true);
 		application.setVisible(true);
+		size = application.getSize();
 
 	}
 	
